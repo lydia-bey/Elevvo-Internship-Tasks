7b5bed521f672f0ba90d955472f8328c1fa8fe2c
lydia <nl_bey@esi.dz>
2025-09-30 20:36:01 +0100

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  const trigger = window.innerHeight * 0.8;
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < trigger) {
+      section.classList.add("show");
+    }
+  });
+});
